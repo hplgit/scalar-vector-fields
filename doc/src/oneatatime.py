e5fde102c8d324c4d248d0491a97cf79f5369873
Øyvind Ryan <oyvindry@math.uio.no>
2015-12-07 16:49:45 +0100

    changes

--- doc/src/oneatatime.py
+++ doc/src/oneatatime.py
@@ -1,26 +1,27 @@
 from math import *
+import numpy as np
 
-from scitools.easyviz import *
+import scitools.easyviz as plt
 
 h0 = 2277.  # Hoyden av toppen av fjellet (m)
 R = 4.      # Maal for radius av fjellet (km)
 
-t = linspace(-10., 10., 41)
+t = np.linspace(-10., 10., 41)
 
-x,y = ndgrid(t,t)            
-h = h0/(1+(x**2+y**2)/(R**2)) 
+x,y = plt.ndgrid(t,t)
+h = h0/(1+(x**2+y**2)/(R**2))
 
 # The following four plots must run one at a time
-mesh(x, y, h, savefig = 'images/simpleplotscitools.png')
+plt.mesh(x, y, h)
+plt.savefig('images/simpleplotscitools.png')
+plt.savefig('images/simpleplotscitools.pdf')
 
-surf(x, y, h)
-hold('on')
-plot3([1,2], [1,2], [1, 2])
-t = linspace(0, 2*pi, 100)
-xcoords = 10*(1 - t/(2*pi))*cos(t)
-ycoords = 10*(1 - t/(2*pi))*sin(t)
-zcoords = h0/(1 + 100*(1 - t/(2*pi))**2/(R**2))
-raw_input('press enter')
-plot3(xcoords, ycoords, zcoords, 'r-')
-raw_input('press enter')
-savefig('images/simpleplotcoloursscitools.png')
+plt.surf(x, y, h)
+plt.hold('on')
+t = np.linspace(0, 2*np.pi, 100)
+xcoords = 10*(1 - t/(2*np.pi))*np.cos(t)
+ycoords = 10*(1 - t/(2*np.pi))*np.sin(t)
+zcoords = h0/(1 + 100*(1 - t/(2*np.pi))**2/(R**2))
+plt.plot3(xcoords, ycoords, zcoords, 'r-')
+plt.savefig('images/simpleplotcoloursscitools.png')
+plt.savefig('images/simpleplotcoloursscitools.pdf')
