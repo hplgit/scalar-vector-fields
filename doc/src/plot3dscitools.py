8e5777a1c338f33c75086e4192272488fa5cbf25
Øyvind Ryan <oyvindry@math.uio.no>
2015-12-07 11:52:33 +0100

    small changes

--- doc/src/plot3dscitools.py
+++ doc/src/plot3dscitools.py
@@ -6,7 +6,6 @@ import scitools.easyviz as plt
 
 #t = np.linspace(0, 10*np.pi, 100)
 #plot3(np.sin(t), np.cos(t), t, title = 'Parametrized curve')
-#raw_input('press enter to continue')
 
 h0 = 2277.  # Hoyden av toppen av fjellet (m)
 R = 4.      # Maal for radius av fjellet (km)
@@ -16,33 +15,26 @@ t = np.linspace(-10., 10., 41)
 x,y = plt.ndgrid(t,t)             # Grid for x- og y-verdiene (km)
 h = h0/(1+(x**2+y**2)/(R**2)) # Beregn hoyden h (m)
 
-
-
 plt.contour(x, y, h)
 plt.savefig('images/simplecontourscitools.pdf')
 plt.savefig('images/simplecontourscitools.png')
-raw_input('press enter to continue')
 
 plt.contour(x, y, h, 10)
 plt.savefig('images/contour10levelsscitools.pdf')
 plt.savefig('images/contour10levelsscitools.png')
-raw_input('press enter to continue')
 
 plt.contour(x, y, h, 10, 'k')
 plt.savefig('images/contour10levelsblackscitools.pdf')
 plt.savefig('images/contour10levelsblackscitools.png')
-raw_input('press enter to continue')
 
 levels = [500., 1000., 1500., 2000.]
 plt.contour(x, y, h, levels=levels)
 plt.savefig('images/contourspeclevelsscitools.pdf')
 plt.savefig('images/contourspeclevelsscitools.png')
-raw_input('press enter to continue')
 
 plt.contour(x, y, h, clabels='on')
 plt.savefig('images/contourclabelscitools.pdf')
 plt.savefig('images/contourclabelscitools.png')
-raw_input('press enter to continue')
 
 t = np.linspace(-5, 5, 11)
 x,y = plt.ndgrid(t, t)
@@ -53,7 +45,6 @@ plt.quiver(x, y, vx, vy, 200, 'b')
 plt.axis('equal')
 plt.savefig('images/quiverscitoolssimple.pdf')
 plt.savefig('images/quiverscitoolssimple.png')
-raw_input('press enter to continue')
 
 
 tt = np.linspace(-10.,10.,11)
@@ -78,4 +69,3 @@ plt.hold('off')
 # Trenger ikke flere plott i denne figuren
 plt.savefig('images/quiverscitoolsadvanced.pdf')
 plt.savefig('images/quiverscitoolsadvanced.png')
-raw_input('Press enter to continue')
