a5c362a448a5cb8872fe440c6d5047aa797cebc0
Øyvind Ryan <oyvindry@math.uio.no>
2015-12-22 10:21:43 +0100

    added fig folder and changes vector field notation

--- doc/src/plot3d_scitools_vtk.py
+++ doc/src/plot3d_scitools_vtk.py
@@ -7,7 +7,7 @@ t2 = np.linspace(.5, 2., 8)
 x, y, z = np.meshgrid(t2, t2, t2)
 r3 = np.sqrt(x**2 + y**2 + z**2)**3
 plt.quiver3(x, y, z, -x/r3, -y/r3, -z/r3, 0, 'r')
-plt.savefig('images/quiverscitoolsgr.png')
-plt.savefig('images/quiverscitoolsgr.pdf')
+plt.savefig('images/quiver_scitools_gr.png')
+plt.savefig('images/quiver_scitools_gr.pdf')
 
 # system python plot3dscitoolsvtk.py --SCITOOLS_easyviz_backend vtk
