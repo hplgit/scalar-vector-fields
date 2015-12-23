693d5525de33049c61702fcb4daf51c96c67a7cc
Øyvind Ryan <oyvindry@math.uio.no>
2015-12-23 13:09:02 +0100

    restructuring

--- doc/src/plot3d_scitools_vtk.py
+++ doc/src/plot3d_scitools_vtk.py
@@ -4,7 +4,7 @@ import numpy as np
 from scitools.easyviz import quiver2, savefig
 
 t2 = np.linspace(.5, 2., 8)
-x, y, z = np.meshgrid(t2, t2, t2)
+x, y, z = plt.ndgrid(t2, t2, t2)
 r3 = np.sqrt(x**2 + y**2 + z**2)**3
 plt.quiver3(x, y, z, -x/r3, -y/r3, -z/r3, 0, 'r')
 plt.savefig('images/quiver_scitools_gr.png')
