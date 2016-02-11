c93e6ce8d8cd0802f2dd9b6cfa2e6b1a53fc307d
Øyvind Ryan <oyvindry@math.uio.no>
2016-02-11 14:44:25 +0100

    added doconce build for matlab and matplotlib notebooks

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -14,7 +14,11 @@ name=programming
 
 system doconce spellcheck -d .dict4spell.txt 3dimplot.do.txt linalg.do.txt
 
+
 system doconce format pdflatex $name --latex_code_style=vrb-blue1
 doconce replace 'section*{Preface}' '' $name.tex
 system pdflatex $name
 pdflatex $name
+
+system doconce format ipynb plot3d_matplotlib.do.txt -D%s --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
+system doconce format matlabnb plot3d_matlabnb.do.txt -D%s --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
