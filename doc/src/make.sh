e057c4b1985998c83a924c39c5d6a1905f6db751
Øyvind Ryan <oyvindry@math.uio.no>
2016-02-12 15:43:29 +0100

    builds matlab and non-matlab version

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -14,13 +14,19 @@ name=programming
 
 system doconce spellcheck -d .dict4spell.txt 3dimplot.do.txt linalg.do.txt
 
-MATLAB_TOO=False
+#MATLAB_TOO=False
 
-system doconce format pdflatex $name --latex_code_style=vrb-blue1 MATLAB=$MATLAB
+#system doconce format pdflatex $name --latex_code_style=vrb-blue1 MATLAB=$MATLAB
+system doconce format pdflatex $name --latex_code_style=vrb-blue1 MATLAB_TOO=True --encoding=utf-8
+doconce replace 'section*{Preface}' '' $name.tex
+system pdflatex $name
+pdflatex $name
+mv $name.pdf programmmingmatlab.pdf
+
+system doconce format pdflatex $name --latex_code_style=vrb-blue1 MATLAB_TOO=False --encoding=utf-8
 doconce replace 'section*{Preface}' '' $name.tex
 system pdflatex $name
 pdflatex $name
 
-# hpl: WHAT DOES -D%s mean?
-system doconce format ipynb plot3d_matplotlib.do.txt  -D%s --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
-system doconce format matlabnb plot3d_matlabnb.do.txt -D%s --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
+system doconce format ipynb plot3d_matplotlib.do.txt  --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
+system doconce format matlabnb plot3d_matlabnb.do.txt --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
