789d8a3f1362185fa406e467c6f6c8633c213ebd
Hans Petter Langtangen <hpl@simula.no>
2016-02-12 11:50:22 +0100

    updates

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -14,11 +14,13 @@ name=programming
 
 system doconce spellcheck -d .dict4spell.txt 3dimplot.do.txt linalg.do.txt
 
+MATLAB_TOO=False
 
-system doconce format pdflatex $name --latex_code_style=vrb-blue1
+system doconce format pdflatex $name --latex_code_style=vrb-blue1 MATLAB=$MATLAB
 doconce replace 'section*{Preface}' '' $name.tex
 system pdflatex $name
 pdflatex $name
 
-system doconce format ipynb plot3d_matplotlib.do.txt -D%s --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
+# hpl: WHAT DOES -D%s mean?
+system doconce format ipynb plot3d_matplotlib.do.txt  -D%s --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
 system doconce format matlabnb plot3d_matlabnb.do.txt -D%s --replace_ref_by_latex_auxno=programming.aux --encoding=utf-8 --allow_refs_to_external_docs
