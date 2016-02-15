6951d7d2dde6023a9c5b218b830c8ddaec37e0b3
Øyvind Ryan <oyvindry@math.uio.no>
2016-02-15 15:27:36 +0100

    final changes to code

--- doc/src/plot3d_mayavi.py
+++ doc/src/plot3d_mayavi.py
@@ -19,29 +19,31 @@ curve_z = h0/(1 + 100*(1 - s/(2*np.pi))**2/(R**2))
 
 # Create a figure with white background and black foreground
 plt.figure(1, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
-plt.mesh(xv, yv, hv, extent=(0,1,0,1,0,1))
-
+# 'representation' controls the plot type, here a wireframe plot.
+plt.surf(xv, yv, hv, extent=(0,1,0,1,0,1), representation='wireframe')
 # Decorate axes (nb_labels is the number of labels in each direction)
 plt.axes(xlabel='x', ylabel='y', zlabel='z', nb_labels=5,
          color=(0., 0., 0.))
-
 # Decorate the plot with a title (size is the size of the title)
 plt.title('h(x,y)', size=0.4)
 
-# Simple plot of mountain and parametric curve
+# Simple plot of mountain and parametric curve.
+# Use 'extent' for auto-scaling
 plt.figure(2, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
-plt.surf(xv, yv, hv, extent=(0,1,0,1,0,1), color=(.5, .5, .5))
-
-# Add the parametric curve (tube_radius is the width of the curve)
+# The default for 'representation' is to color the surface elements.
+plt.surf(xv, yv, hv, extent=(0,1,0,1,0,1))
+# Add the parametric curve. tube_radius is the width of the curve
 plt.plot3d(curve_x, curve_y, curve_z, tube_radius=0.2,
            extent=(0,1,0,1,0,1))
-plt.axes(xlabel='x', ylabel='y', zlabel='z', nb_labels=5,
-         color=(0., 0., 0.))
+
+# Use 'warp_scale' for vertical scaling
+plt.figure(3, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
+plt.surf(xv, yv, hv, warp_scale=0.01, color=(.5, .5, .5))
+plt.plot3d(curve_x, curve_y, 0.01*curve_z, tube_radius=0.2)
 # endsimpleplots
 
-R2 = 10.
 # Create one figure with three subplots
-plt.figure(3, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
+plt.figure(4, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
 plt.mesh(xv, yv, hv, extent=(0, 0.25, 0, 0.25, 0, 0.25),
          colormap='cool')
 plt.outline(plt.mesh(
@@ -53,33 +55,32 @@ plt.outline(plt.mesh(
     colormap='prism'), color=(.5, .5, .5))
 # endsubplot
 
-h0 = 22.77
-R = 4.
 
 hv = h0/(1 + (xv**2+yv**2)/(R**2))
 
 # Default contour plot plotted together with surf.
-plt.figure(4, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
-plt.surf(xv, yv, hv)
-plt.contour_surf(xv, yv, hv)
+plt.figure(5, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
+plt.surf(xv, yv, hv, warp_scale=0.01)
+plt.contour_surf(xv, yv, hv, warp_scale=0.01)
 
 # 10 contour lines (equally spaced contour levels).
-plt.figure(5, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
-plt.contour_surf(xv, yv, hv, contours=10)
+plt.figure(6, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
+plt.contour_surf(xv, yv, hv, contours=10, warp_scale=0.01)
 
 # 10 contour lines (equally spaced contour levels) together with surf.
 # Black color for contour lines.
-plt.figure(6, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
-plt.surf(xv, yv, hv)
-plt.contour_surf(xv, yv, hv, contours=10, color=(0., 0., 0.))
+plt.figure(7, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
+plt.surf(xv, yv, hv, warp_scale=0.01)
+plt.contour_surf(xv, yv, hv, contours=10, color=(0., 0., 0.), \
+                 warp_scale=0.01)
 
 # Specify the contour levels explicitly as a list.
-plt.figure(7, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
-levels = [5., 10., 15., 20.]
-plt.contour_surf(xv, yv, hv, contours=levels)
+plt.figure(8, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
+levels = [500., 1000., 1500., 2000.]
+plt.contour_surf(xv, yv, hv, contours=levels, warp_scale=0.01)
 
 # View the contours by displaying as an image.
-plt.figure(8, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
+plt.figure(9, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
 plt.imshow(hv)
 #end contourplots
 
@@ -97,7 +98,7 @@ dhdx, dhdy = np.gradient(h2v)
 
 # Create animation
 plt.figure(13, fgcolor=(.0, .0, .0), bgcolor=(1.0, 1.0, 1.0))
-s = plt.surf(xv, yv, hv)
+s = plt.surf(xv, yv, hv, warp_scale=0.01)
 
 for i in range(10):
     # s.mlab_source.scalars is a handle for the values of the surface,
@@ -108,9 +109,9 @@ for i in range(10):
 
 # Define grid for 3D scalar field
 x = y = np.linspace(-10.,10.,41)
-z = np.linspace(0,50, 41)
+z = np.linspace(0, 50, 41)
 xv, yv, zv = np.meshgrid(x, y, z, sparse=False, indexing='ij')
-hv = h0/(1 + (xv**2+yv**2)/(R**2))
+hv = 0.01*h0/(1 + (xv**2+yv**2)/(R**2))
 gv = zv - hv
 # end define grid for 3D scalar field
 
@@ -118,7 +119,7 @@ gv = zv - hv
 x2 = y2 = np.linspace(-10.,10.,5)
 z2 = np.linspace(0, 50, 5)
 x2v, y2v, z2v = np.meshgrid(x2, y2, z2, indexing='ij', sparse=False)
-h2v = h0/(1 + (x2v**2 + y2v**2)/(R**2))
+h2v = 0.01*h0/(1 + (x2v**2 + y2v**2)/(R**2))
 g2v = z2v - h2v
 dhdx, dhdy, dhdz = np.gradient(g2v)
 # end define grid for 3D gradient field
@@ -140,23 +141,26 @@ plt.figure(2)
 plt.savefig('images/simple_plot_colours_mayavi.png')
 
 plt.figure(3)
+plt.savefig('images/simple_plot_colours_mayavi_2.png')
+
+plt.figure(4)
 plt.savefig('images/subplot.png')
 
 # Save contours plots
 
-plt.figure(4)
+plt.figure(5)
 plt.savefig('images/simple_contour_mayavi.png')
 
-plt.figure(5)
+plt.figure(6)
 plt.savefig('images/contour_10levels_mayavi.png')
 
-plt.figure(6)
+plt.figure(7)
 plt.savefig('images/contour_10levels_black_mayavi.png')
 
-plt.figure(7)
+plt.figure(8)
 plt.savefig('images/contour_speclevels_mayavi.png')
 
-plt.figure(8)
+plt.figure(9)
 plt.savefig('images/contour_imshow_mayavi.png')
 
 # Save vector field plots
